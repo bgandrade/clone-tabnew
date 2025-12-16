a22bf3a97d3070217603096c97f296573dc97b8b
Bruno Andrade <brungomes@msn.com>
2025-12-15 21:56:40 -0300

    fix `api/v1/migrations` connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,33 +3,46 @@ import database from "infra/database";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const client = await database.getNewClient();
-  const migrationDefaultOptions = {
-    dbClient: client,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(migrationDefaultOptions);
-    client.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...migrationDefaultOptions,
-      dryRun: false,
+  const allowedMethod = ["GET", "POST"];
+  if (!allowedMethod.includes(request.method)) {
+    return response.status(405).json({
+      error: `ther method ${request.method} is not allowed`,
     });
-    client.end();
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405).end();
+  let client;
+  try {
+    client = await database.getNewClient();
+    const migrationDefaultOptions = {
+      dbClient: client,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(migrationDefaultOptions);
+
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...migrationDefaultOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    client.end();
+  }
 }
