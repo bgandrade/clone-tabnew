66591b9dc061b2831a3944c5902104ca36152606
Bruno Andrade <brungomes@msn.com>
2026-02-11 21:34:06 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -5,14 +5,17 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
+describe("Post /api/v1/migrations", () => {
+  describe("Anonymus user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-test("Get to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
 
-  const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
 
-  expect(Array.isArray(responseBody)).toBe(true);
-
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
